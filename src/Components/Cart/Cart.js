afddbb152046904bb4b0f578d35ee2c80251940b
raselASrock <mdraselm413@gmail.com>
2022-10-08 11:08:17 +0600

    ternary

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 const Cart = ({cart, handleRemoveItem}) => {
+
+    let message;
+    if(cart.length === 0){
+        message = <p>Please Buy Atleast One Item.</p>
+    }
+    else if(cart.length === 1){
+        message = <div>
+            <h2>Ammar jonno 1ta nau.</h2>
+            <p><small>Sobar jonnoi.</small></p>
+        </div>
+    }
+    else{
+        message = <p>Thanks For Buying.</p>
+    }
     return (
         <div>
             <h2>Order Summary</h2>
@@ -13,8 +27,19 @@ const Cart = ({cart, handleRemoveItem}) => {
                 <button onClick={() => handleRemoveItem(tshirt)}>X</button>
                 </p>)
             }
+            {
+                message
+            }
+            {cart.length === 3 ? <p>Tin jon ke gift diba?</p> : <p>Kino Kino....</p> }
         </div>
     );
 };
 
 export default Cart;
+
+
+/* 
+Conditional Rendaring
+1. use element in a veriable  and then use if-else to set value.
+2. ternary operator  consition ? true : false
+*/
