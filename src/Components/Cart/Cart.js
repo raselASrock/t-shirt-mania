e54a0bcb27ab286178fbaa852d3bab5af28e3092
raselASrock <mdraselm413@gmail.com>
2022-10-08 11:26:00 +0600

    condition css

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ('./Cart.css')
 
 const Cart = ({cart, handleRemoveItem}) => {
 
@@ -17,8 +18,8 @@ const Cart = ({cart, handleRemoveItem}) => {
     }
     return (
         <div>
-            <h2>Order Summary</h2>
-            <h4>Order Quantity: {cart.length}</h4>
+            <h2 className={cart.length === 2 ? `orange`:`purple`}>Order Summary</h2>
+            <h4 className={`bold ${cart.length === 2 ? "blue" : "yellow"}`}>Order Quantity: {cart.length}</h4>
             {
                 cart.map( tshirt => <p
                 key={tshirt._id}
@@ -31,6 +32,10 @@ const Cart = ({cart, handleRemoveItem}) => {
                 message
             }
             {cart.length === 3 ? <p>Tin jon ke gift diba?</p> : <p>Kino Kino....</p> }
+            <p>And Operator</p>
+            {cart.length === 2 && <h2>Double Item</h2> } 
+            <p>Or Operator</p>
+            {cart.length === 4 || <h5>4 Items Not Select</h5> }
         </div>
     );
 };
@@ -42,4 +47,6 @@ export default Cart;
 Conditional Rendaring
 1. use element in a veriable  and then use if-else to set value.
 2. ternary operator  consition ? true : false
+3. && and operator (if condition is true, it show its value.)
+4. || or operator (if condition is false, it show its value.)
 */
