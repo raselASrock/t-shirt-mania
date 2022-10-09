e2f57b21892eacb4923c32c4053b5468fcf81698
raselASrock <mdraselm413@gmail.com>
2022-10-09 23:11:19 +0600

    apply

--- src/App.js
+++ src/App.js
@@ -56,6 +56,33 @@ export default App;
         {
           path: '/grandPa',
           element: <GrandPa></GrandPa>
+          import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import GrandPa from './Components/GrandPa/GrandPa';
+import Home from './Components/Home/Home';
+import Orders from './Components/Orders/Orders';
+import Main from './Layouts/Main';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('tshirts.json'),
+          element: <Home></Home>
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>
+        },
+        {
+          path: '/grandPa',
+          element: <GrandPa></GrandPa>
+
+
 
 */
 
